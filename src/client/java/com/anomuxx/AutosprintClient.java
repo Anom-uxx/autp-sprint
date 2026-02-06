94bbb39822c6dcf043bc486941392a02654eaa00
Anom-uxx <225215532+Anom-uxx@users.noreply.github.com>
2026-02-06 20:45:16 +0100

    first commit

---
+++ src/client/java/com/anomuxx/AutosprintClient.java
@@ -0,0 +1,10 @@
+package com.anomuxx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutosprintClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
